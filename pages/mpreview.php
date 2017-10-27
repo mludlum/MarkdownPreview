5e61be613d18fa05206b593054d69b2693355392
mludlum <mludlum@usefulsolutions.com>
2017-10-27 11:49:39 -0400

    added timer to update every 5 seconds

--- pages/mpreview.php
+++ pages/mpreview.php
@@ -4,26 +4,39 @@ header('Content-Type: text/javascript');
 
 ?>
 $(document).ready(function() {
-	$('textarea.form-control').each(function(index, el) {
-		var $el = $(el);
-		var mpId = 'mp-'+el.id;
-		var html = '<div id="'+mpId+'" class="markdown-preview" data="'+el.id+'"></div>';
-		$el.parent().append(html);
-		$el.blur(function() {
-			var data =  {markdown: $el.val()};
-			var $target = $('#'+mpId);
-			$.ajax({
-			  url: '<?php echo plugin_page('mpconvert.php') ?>',
-			  data: data,
-			  dataType: 'html' 
-			}).done(function(data) {
-				$target.html(data);
-			})
-			.fail(function() {
-			   alert( "error" );
-			});
-			//.always(function() {
-  			//});
-		});
-	});
+        function updatePreview(mpId, $el)
+        {
+                        var data =  {markdown: $el.val()};
+                        var $target = $('#'+mpId);
+                        $.ajax({
+                          url: '<?php echo plugin_page('mpconvert.php') ?>',
+                          data: data,
+                          dataType: 'html'
+                        }).done(function(data) {
+                                $target.html(data);
+                        })
+                        .fail(function() {
+                           alert( "error" );
+                        });
+                        //.always(function() {
+                        //});
+        }
+        $('textarea.form-control').each(function(index, el) {
+                var $el = $(el);
+                var mpId = 'mp-'+el.id;
+                var timerId = undefined;
+                var html = '<div id="'+mpId+'" class="markdown-preview" data="'+el.id+'"></div>';
+                $el.parent().append(html);
+                $el.keyup(function() {
+                        if (timerId === undefined) {
+                                timerId = setTimeout(function() {
+                                        updatePreview(mpId, $el);
+                                        timerId = undefined;
+                                }, 5000)
+                        }
+                });
+                $el.blur(function() {
+                        updatePreview(mpId, $el);
+                });
+        });
 });
