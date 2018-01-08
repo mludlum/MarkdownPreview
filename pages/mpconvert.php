fdba1b5fdfffe8723ecb2b1979085dbb49f6c296
mludlum <mludlum@usefulsolutions.com>
2018-01-08 15:11:31 -0500

    mantis version 2.1 broke require_lib
    including directly from vendor path

--- pages/mpconvert.php
+++ pages/mpconvert.php
@@ -4,12 +4,12 @@ global $g_vendor_path;
 $parsedown_sub = 'parsedown'.DIRECTORY_SEPARATOR.'Parsedown.php';
 if (file_exists($g_vendor_path.DIRECTORY_SEPARATOR.$parsedown_sub))
 {
-        require_lib($parsedown_sub);
+        require_once($g_vendor_path.DIRECTORY_SEPARATOR.$parsedown_sub);
 }
 // version 2.7
 else if (file_exists($g_vendor_path.DIRECTORY_SEPARATOR.'erusev'.DIRECTORY_SEPARATOR.$parsedown_sub))
 {
-        require_lib('erusev'.DIRECTORY_SEPARATOR.$parsedown_sub);
+        require_once($g_vendor_path.DIRECTORY_SEPARATOR.'erusev'.DIRECTORY_SEPARATOR.$parsedown_sub);
 }
 else
         die('<em style="color:red">parsedown library not available.</em>');
